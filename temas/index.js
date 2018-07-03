914af3e4887bc1f829fb9bfe27f7db46ead9f32c
Holkan <luah613@gmail.com>
2018-07-03 13:47:24 -0500

    carousel ok & bg at body

--- temas/index.js
+++ temas/index.js
@@ -14,7 +14,7 @@
     	return this.charAt(0).toUpperCase() + this.slice(1);
 	}
 
-	var parameter = getUrlParameter('ref');
+	var parameter = getUrlParameter('refID');
 	var parameterNme = getUrlParameter('refnme');
 
 	var themeTitle = document.getElementById('theme_title');
@@ -22,28 +22,46 @@
 	// themeTitle.textContent = parameterNme.capitalize();
 	themeTitle.textContent = parameterNme.toUpperCase();
 
-	const mysiema = new Siema({
-	    selector: '.siema',
-	    duration: 200,
-	    easing: 'ease-out',
-	    perPage: 5,
-	    startIndex: 0,
-	    draggable: true,
-	    multipleDrag: true,
-	    threshold: 20,
-	    loop: false,
-	    rtl: false,
-	});
+	/*AJAX FOR VIDEOS BY THEME*/
+	/*
+		AJAX FOR TEMA
+	*/
+	var xmlhttp = new XMLHttpRequest();
+	// var url = 'https://forotv.mx/wp-json/forotv/v1/topicos/';
+	var url = 'https://localhost/forotvmx/wp-json/forotv/v1/topic/'+parameter;
+	var jsonArr = '';
 
-	var btnNext = document.getElementById('right-btn');
-	btnNext.addEventListener('click', ()=> mysiema.next());
+	xmlhttp.onreadystatechange = function(){
+		if(this.readyState == 4 && this.status == 200){
+			jsonArr = JSON.parse(this.responseText);
+			console.log(jsonArr);
+			// buildThemeList(jsonArr);
+			// document.onload = loadSiema();
+		}
+	};
+	xmlhttp.open('GET', url, true);
+	xmlhttp.send();
 
-	var btnPrev = document.getElementById('left-btn');
-	btnPrev.addEventListener('click', () => mysiema.prev());
+	function loadSiema(){
+		const mysiema = new Siema({
+		    selector: '.siema',
+		    duration: 200,
+		    easing: 'ease-out',
+		    perPage: 5,
+		    startIndex: 0,
+		    draggable: true,
+		    multipleDrag: true,
+		    threshold: 20,
+		    loop: false,
+		    rtl: false,
+		});
 
-	// var btnReturn = document.getElementById('btn_return');
-	// btnReturn.addEventListener('click', function(){
-	// 	window.history.back();
-	// });
+		var btnNext = document.getElementById('right-btn');
+		btnNext.addEventListener('click', ()=> mysiema.next());
+
+		var btnPrev = document.getElementById('left-btn');
+		btnPrev.addEventListener('click', () => mysiema.prev());
+	}
+
 	
 })();
